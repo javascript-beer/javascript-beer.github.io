4e8ee44914517b577b44a9e38dd1c351995b3977
Felix Zapata <fzberlinches@gmail.com>
2021-04-04 14:44:37 +0200

    feat(dist): update bundled

--- dist/_next/static/chunks/webpack.js
+++ dist/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0ececdd0bdcf6b696356";
+/******/ 	var hotCurrentHash = "df4be78e7d82da4fb7ae";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
